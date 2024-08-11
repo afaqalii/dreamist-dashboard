bf932c90b48062bccd94f5734a1d6679199974f8
Afaq <afaqali4244@gmail.com>
2024-08-11 20:11:22 +0500

    Done with dashboard

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,20 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import Accordian from './Accordian';
 import Spinner from '@/components/ui/loader/loader';
 
 function Orders() {
-  const { orders, isLoading } = useSelector(state => state.orders)
-
+  const [orders, setOrder] = useState([]);
   return (
     <div className="text-white p-4">
       <h1>{orders.length} Orders have been placed</h1>
       {
-        isLoading ? <Spinner />
+        true ? <Spinner />
           : orders.length > 0 ?
-            orders.map((order) => (
-              <Accordian key={order?.id || order.items[0].id} order={order} />
-            ))
+          <></>
+            // orders.map((order) => (
+            //   <Accordian key={order?.id || order.items[0].id} order={order} />
+            // ))
             : <h1>No orders yet</h1>
       }
     </div>
