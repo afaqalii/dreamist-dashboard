927ed32c3b4171ffbcab57eb6169278174f606c8
Afaq <afaqali4244@gmail.com>
2024-08-28 06:12:21 +0500

    changed firebase source

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'instagram.fpew1-1.fna.fbcdn.net',
-            },
-            {
-                protocol: 'https',
-                hostname: 'firebasestorage.googleapis.com',
-                pathname: '/v0/b/dreamist-dashboard.appspot.com/**', // Allow all paths under this Firebase storage bucket
-            },
-        ],
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'instagram.fpew1-1.fna.fbcdn.net',
+        },
+        {
+          protocol: 'https',
+          hostname: 'firebasestorage.googleapis.com',
+          pathname: '/v0/b/dreamistclothing1.appspot.com/**', // Updated to match your Firebase bucket
+        },
+      ],
     },
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
+  
